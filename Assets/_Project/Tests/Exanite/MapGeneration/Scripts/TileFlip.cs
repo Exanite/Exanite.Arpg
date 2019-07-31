8455230d47ac1b7bf13e861699a269b06268d62d
Exanite <Exanite78012@gmail.com>
2019-07-31 07:39:31 -0700

    Added the start of a Map Generation system
    Former-commit-id: f942051d01ebfcf01dda92835678f57c2e1ed597

---
+++ Assets/_Project/Tests/Exanite/MapGeneration/Scripts/TileFlip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exanite.MapGeneration
+{
+    [Flags, Serializable]
+    public enum TileFlip
+    {
+        Normal = 0 << 0,
+        FlipX = 1 << 0,
+        FlipZ = 1 << 1,
+        FlipXZ = FlipX | FlipZ,
+    }
+}
---
+++ Assets/_Project/Tests/Exanite/MapGeneration/Scripts/TileSide.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exanite.MapGeneration
+{
+    [Serializable]
+    public enum TileSide
+    {
+        PositiveZ = 0,
+        PositiveX = 1,
+        NegativeZ = 2,
+        NegativeX = 3,
+    }
+}
