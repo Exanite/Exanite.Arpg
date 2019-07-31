259ea24e190dd621ce29e01d1cab3646ab4bc25d
Exanite <Exanite78012@gmail.com>
2019-07-31 07:39:31 -0700

    Added the start of a Map Generation system
    Former-commit-id: f942051d01ebfcf01dda92835678f57c2e1ed597

---
+++ Assets/_Project/Tests/Exanite/MapGeneration/Scripts/Map.cs
@@ -0,0 +1,39 @@
+﻿using Exanite.Core.DataStructures;
+using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+namespace Exanite.MapGeneration
+{
+    public class Map : SerializedMonoBehaviour
+    {
+        private Grid2D<GameObject> Tiles { get; set; }
+
+        /// <summary>
+        /// Creates a Tile at (<paramref name="x"/>, <paramref name="y"/>) <para/> Note: '<paramref name="z"/>' is height
+        /// </summary>
+        public GameObject CreateTileAt(GameObject prefab, int x, int y, int z = 0)
+        {
+            if (Tiles[x, y] != null)
+            {
+                throw new ArgumentException($"Unable to create tile, a tile already exists at ({x}, {y})");
+            }
+
+            var tile = Instantiate(prefab);
+            tile.transform.SetParent(transform, true);
+            tile.transform.localPosition = new Vector3(x, z, y);
+
+            return tile;
+        }
+
+        public void Clear()
+        {
+            foreach (var tile in Tiles)
+            {
+                Destroy(tile);
+            }
+
+            Tiles.Clear();
+        }
+    } 
+}
---
+++ Assets/_Project/Tests/Exanite/MapGeneration/Scripts/Tile.cs
@@ -0,0 +1,69 @@
+﻿using Exanite.MapGeneration.Extensions;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System;
+
+namespace Exanite.MapGeneration
+{
+    public class Tile : SerializedMonoBehaviour
+    {
+        public const int Sides = 4;
+        public const int ConnectionsPerSide = 3;
+
+        [ShowInInspector, EnumToggleButtons, OdinSerialize] public TileFlip Flip { get; set; }
+        [ShowInInspector, EnumPaging, OdinSerialize] public TileRotation Rotation { get; set; }
+
+        [OdinSerialize] public bool[,] Connections { get; set; } = new bool[Sides, ConnectionsPerSide];
+
+        public bool GetConnection(TileSide side, int connection)
+        {
+            var indexes = GetConnectionIndexes(side, connection);
+
+            return Connections[indexes.x, indexes.y];
+        }
+
+        public void SetConnection(bool value, TileSide side, int connection)
+        {
+            var indexes = GetConnectionIndexes(side, connection);
+
+            Connections[indexes.x, indexes.y] = value;
+        }
+
+        public (int x, int y) GetConnectionIndexes(TileSide side, int connection)
+        {
+            if (connection < 0 || connection >= ConnectionsPerSide)
+            {
+                throw new ArgumentOutOfRangeException(nameof(connection));
+            }
+
+            bool reverseConnection = false;
+
+            side = side.Rotate(Rotation);
+
+            if (Flip.HasFlag(TileFlip.FlipX))
+            {
+                side = side.FlipX();
+
+                reverseConnection = !reverseConnection;
+            }
+
+            if (Flip.HasFlag(TileFlip.FlipZ))
+            {
+                side = side.FlipZ();
+
+                reverseConnection = !reverseConnection;
+            }
+
+            if (reverseConnection)
+            {
+                switch (connection)
+                {
+                    case 0: connection = 2; break;
+                    case 2: connection = 0; break;
+                }
+            }
+
+            return ((int)side, connection);
+        }
+    }
+}
