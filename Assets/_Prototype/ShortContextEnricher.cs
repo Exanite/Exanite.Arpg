b36fcb251ee8a5160ba2fd449b7ac58714aa258c
Exanite <Exanite78012@gmail.com>
2020-01-19 18:54:33 -0800

    Added ShortContextEnricher to the Logger. This formats the SourceContext to a shorter version without the preceding namespaces for better readability.
    Former-commit-id: 7eb97058fdc1c7aeea9ba5e969c5b58b50dc3e5a

---
+++ Assets/_Prototype/ShortContextEnricher.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Prototype
+{
+    public class ShortContextEnricher : ILogEventEnricher
+    {
+        public const string SourceContext = "SourceContext";
+        public const string ShortContext = "ShortContext";
+        public const string DefaultValue = "Default";
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            string shortened = GetShortened(logEvent.Properties[SourceContext]);
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(ShortContext, shortened));
+        }
+
+        public string GetShortened(LogEventPropertyValue property)
+        {
+            if (property == null)
+            {
+                return DefaultValue;
+            }
+
+            using (StringWriter writer = new StringWriter())
+            {
+                property.Render(writer);
+
+                string original = writer.ToString().Trim('"');
+                int startIndex = original.LastIndexOf('.') + 1;
+
+                return original.Substring(startIndex);
+            }
+        }
+    }
+}
