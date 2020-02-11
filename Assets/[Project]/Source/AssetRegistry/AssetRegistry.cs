8d914299f53a9f7d7222f4523563868eaa84e0dc
Exanite <Exanite78012@gmail.com>
2020-02-11 14:49:16 -0800

    Moved AssetRegistry into the actual project instead of being a submodule.

---
+++ Assets/[Project]/Source/AssetRegistry/AssetRegistry.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Exanite.Arpg.AssetRegistry.Providers;
+
+namespace Exanite.Arpg.AssetRegistry
+{
+    public class AssetRegistry
+    {
+        private readonly Dictionary<Key, AssetEntry> entries;
+
+        public AssetRegistry()
+        {
+            entries = new Dictionary<Key, AssetEntry>();
+        }
+
+        public void AddEntry<T>(Key key, AssetEntry<T> entry)
+        {
+            entries.Add(key, entry);
+        }
+
+        public void RemoveEntry(Key key)
+        {
+            entries.Remove(key);
+        }
+
+        public AssetProvider<T> GetProvider<T>(Key key)
+        {
+            return GetEntry<T>(key).TypedProvider;
+        }
+
+        public AssetEntry<T> GetEntry<T>(Key key)
+        {
+            if (entries.ContainsKey(key))
+            {
+                return (AssetEntry<T>)entries[key];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/[Project]/Source/AssetRegistry/Key.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Exanite.Arpg.AssetRegistry
+{
+    public struct Key : IEquatable<Key>
+    {
+        private readonly string value;
+
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public Key(string value)
+        {
+            this.value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public static implicit operator Key(string value)
+        {
+            return new Key(value);
+        }
+
+        public static bool operator ==(Key lhs, Key rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Key lhs, Key rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        public bool Equals(Key other)
+        {
+            return value.Equals(other.value, StringComparison.Ordinal);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Key key)
+            {
+                return Equals(key);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
