c562725bf8bbc9739a673c5e5675e62bcb193a5e
Exanite <Exanite78012@gmail.com>
2020-02-11 18:01:57 -0800

    Created a ModInfo class for holding mod data.

---
+++ Assets/[Project]/Source/Modding/ModInfo.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.Modding
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ModInfo : ScriptableObject
+    {
+        public const string AssetsFolderName = "Assets";
+        public const string BundlesFolderName = "Bundles";
+        public const string SourceCodeFolderName = "Source";
+
+        [SerializeField, HideInInspector] private string modID;
+        [SerializeField, HideInInspector] private string modName;
+        [SerializeField, HideInInspector] private string modDescription;
+        [SerializeField, HideInInspector] private string modVersion;
+
+        [JsonProperty]
+        [ShowInInspector]
+        public string ModID
+        {
+            get
+            {
+                return modID;
+            }
+
+            set
+            {
+                modID = value;
+            }
+        }
+
+        [JsonProperty]
+        [ShowInInspector]
+        public string ModName
+        {
+            get
+            {
+                return modName;
+            }
+
+            set
+            {
+                modName = value;
+            }
+        }
+
+        [JsonProperty]
+        [ShowInInspector]
+        public string ModDescription
+        {
+            get
+            {
+                return modDescription;
+            }
+
+            set
+            {
+                modDescription = value;
+            }
+        }
+
+        [JsonProperty]
+        [ShowInInspector]
+        public string ModVersion
+        {
+            get
+            {
+                return modVersion;
+            }
+
+            set
+            {
+                modVersion = value;
+            }
+        }
+    } 
+}
