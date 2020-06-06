a2e848bc5eb2b52682d09a36453ef5716b11f7dd
Exanite <Exanite78012@gmail.com>
2020-06-06 00:43:10 -0700

    Moved most of the code out of the Source folder and moved them into Exanite.Arpg at the root of the Assets folder. This is so Visual Studio properly generates correct namespaces for files and keeps things more organized. Also added Assembly definitions to each sub-project.
    Former-commit-id: 8492b855a8be4e4fc2f9ef75b29932f24bc65013

---
+++ Assets/Exanite.Arpg/AssetManagement/Registry/AssetEntry.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exanite.Arpg.AssetRegistry
+{
+    public class AssetEntry
+    {
+        private string path;
+        private string package;
+        private Type type;
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+
+            set
+            {
+                path = value;
+            }
+        }
+
+        public string Package
+        {
+            get
+            {
+                return package;
+            }
+
+            set
+            {
+                package = value;
+            }
+        }
+
+        public Type Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+    }
+}
