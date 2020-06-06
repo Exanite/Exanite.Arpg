99c99b14119c2e705ad9280aaa1112a65659a93d
Exanite <Exanite78012@gmail.com>
2020-06-06 00:45:58 -0700

    Added the AssetBundleBuilder class. This builds an AssetBundle and creates a list of all assets in the bundle and their types. The .bundleinfo file can be deserialized into the AssetBundleInfo class. Also added a new Assembly Definition for the AssetManagement.Editor namespace.
    Former-commit-id: 7abc7803dbb832e2591ad86a47b83113d6a8ba05

---
+++ Assets/Exanite.Arpg/AssetManagement/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using UnityEditor;
+
+namespace Exanite.Arpg.AssetManagement.AssetBundles
+{
+    public class AssetBundleBuilder
+    {
+        [MenuItem("PackageBuilder/Test Build")]
+        public static void TestBuild()
+        {
+            Build("examplemod", "Assets/Mods/ExampleMod/Assets", @"D:\Repositories\Exanite.Arpg\Assets\Mods\ExampleMod\Bundles");
+        }
+
+        public static void Build(string packageName, string assetFolder, string buildDirectory)
+        {
+            packageName = packageName.ToLower();
+            var assetNames = GetAssetNamesForDirectory(assetFolder);
+            var addressableNames = FormatAddressableNames(assetFolder, assetNames);
+            var assetBundleInfo = BuildAssetBundleInfo(assetNames, addressableNames);
+
+            var build = new AssetBundleBuild[1];
+            build[0].assetBundleName = packageName;
+            build[0].assetNames = assetNames;
+            build[0].addressableNames = addressableNames;
+
+            var buildOptions = BuildAssetBundleOptions.DisableLoadAssetByFileName
+                | BuildAssetBundleOptions.DisableLoadAssetByFileNameWithExtension;
+
+            BuildPipeline.BuildAssetBundles(buildDirectory, build, buildOptions, BuildTarget.StandaloneWindows64);
+
+            var json = JsonConvert.SerializeObject(assetBundleInfo, Formatting.Indented);
+            File.WriteAllText(Path.Combine(buildDirectory, $"{packageName}.{AssetBundleInfo.FileExtension}"), json);
+
+            AssetDatabase.Refresh();
+        }
+
+        private static string[] GetAssetNamesForDirectory(string assetFolder)
+        {
+            return AssetDatabase.FindAssets("", new[] { assetFolder })
+                .Select(x => AssetDatabase.GUIDToAssetPath(x))
+                .Distinct()
+                .Where(x => AssetDatabase.GetMainAssetTypeAtPath(x) != typeof(DefaultAsset))
+                .ToArray();
+        }
+
+        private static string[] FormatAddressableNames(string assetFolder, string[] assetNames)
+        {
+            assetFolder = assetFolder.Trim('/');
+
+            string[] addressableNames = new string[assetNames.Length];
+
+            for (int i = 0; i < assetNames.Length; i++)
+            {
+                string current = assetNames[i];
+
+                current = current.Remove(0, assetFolder.Length + 1);
+                current = Path.ChangeExtension(current, null);
+
+                addressableNames[i] = current;
+            }
+
+            return addressableNames;
+        }
+
+        private static AssetBundleInfo BuildAssetBundleInfo(string[] assetNames, string[] addressableNames)
+        {
+            if (assetNames.Length != addressableNames.Length)
+            {
+                throw new ArgumentException($"'{nameof(assetNames)}' and '{nameof(addressableNames)}' must have the same length");
+            }
+
+            var result = new AssetBundleInfo(assetNames.Length);
+            AssetBundleEntry entry;
+
+            for (int i = 0; i < assetNames.Length; i++)
+            {
+                entry = new AssetBundleEntry
+                {
+                    Key = addressableNames[i],
+                    AssetType = AssetDatabase.GetMainAssetTypeAtPath(assetNames[i]),
+                };
+
+                result.Entries.Add(entry);
+            }
+
+            return result;
+        }
+    }
+}
