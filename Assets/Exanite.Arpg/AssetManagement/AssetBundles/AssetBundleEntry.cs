0413f5dd78164d6dc5507fe2d6839f8608b9c1e2
Exanite <Exanite78012@gmail.com>
2020-06-06 00:44:26 -0700

    Added a two classes that will hold info about the content of an AssetBundle
    Former-commit-id: eb1b1b75c106197e9bd43e625c09e90940c56bc5

---
+++ Assets/Exanite.Arpg/AssetManagement/AssetBundles/AssetBundleEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exanite.Arpg.AssetManagement.AssetBundles
+{
+    public class AssetBundleEntry
+    {
+        private string key;
+        private Type assetType;
+
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+
+        public Type AssetType
+        {
+            get
+            {
+                return assetType;
+            }
+
+            set
+            {
+                assetType = value;
+            }
+        }
+    }
+}
