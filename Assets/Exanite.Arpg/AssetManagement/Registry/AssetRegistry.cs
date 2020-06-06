8492b855a8be4e4fc2f9ef75b29932f24bc65013
Exanite <Exanite78012@gmail.com>
2020-06-06 00:43:10 -0700

    Moved most of the code out of the Source folder and moved them into Exanite.Arpg at the root of the Assets folder. This is so Visual Studio properly generates correct namespaces for files and keeps things more organized. Also added Assembly definitions to each sub-project.

---
+++ Assets/Exanite.Arpg/AssetManagement/Registry/AssetRegistry.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Exanite.Arpg.AssetRegistry
+{
+    public class AssetRegistry
+    {
+        private readonly Dictionary<Key, AssetEntry> entries;
+        private readonly Dictionary<Key, AssetBundle> bundles;
+
+        public Dictionary<Key, AssetEntry> Entries
+        {
+            get
+            {
+                return entries;
+            }
+        }
+
+        public Dictionary<Key, AssetBundle> Bundles
+        {
+            get
+            {
+                return bundles;
+            }
+        }
+    }
+}
---
+++ Assets/Exanite.Arpg/Modding/Editor/Modding/ModInfoEditor.cs
@@ -0,0 +1,76 @@
+﻿//using System;
+//using System.IO;
+//using Exanite.Core.Extensions;
+//using Sirenix.OdinInspector.Editor;
+//using UnityEditor;
+//using UnityEngine;
+
+//namespace Exanite.Arpg.Modding.Editor
+//{
+//    [CustomEditor(typeof(ModInfo))]
+//    public class ModInfoEditor : OdinEditor
+//    {
+//        public ModInfo TypedTarget;
+
+//        protected override void OnEnable()
+//        {
+//            TypedTarget = target as ModInfo;
+//        }
+
+//        public override void OnInspectorGUI()
+//        {
+//            base.OnInspectorGUI();
+
+//            EditorGUILayout.BeginVertical();
+//            {
+//                if (AssetDatabase.Contains(TypedTarget))
+//                {
+//                    DrawButton("Set up mod folders", () => ModUtility.SetUpModFolders(TypedTarget));
+
+//                    DrawButton("Build mod", () => BuildMod());
+//                }
+//            }
+//            EditorGUILayout.EndVertical();
+//        }
+
+//        private void DrawButton(string name, Action action)
+//        {
+//            if (GUILayout.Button(name))
+//            {
+//                action?.Invoke();
+//            }
+//        }
+
+//        private void BuildMod()
+//        {
+//            ModUtility.CreateDefaultModBuildPath();
+
+//            string buildPath = EditorUtility.SaveFolderPanel("Choose mod build directory", ModUtility.DefaultModBuildPath, TypedTarget.ModID);
+
+//            if (string.IsNullOrWhiteSpace(buildPath))
+//            {
+//                return;
+//            }
+
+//            DirectoryInfo buildDirectory = new DirectoryInfo(buildPath);
+
+//            if (!buildDirectory.IsEmpty())
+//            {
+//                if (!EditorUtility.DisplayDialog(
+//                    "Overwrite directory?",
+//                    "Pressing 'Overwrite' will overwrite all contents of this directory. Are you sure you want to continue?",
+//                    "Overwrite",
+//                    "Cancel"))
+//                {
+//                    return;
+//                }
+//            }
+
+//            Debug.Log($"Starting build of {TypedTarget.ModID}");
+
+//            ModUtility.Build(TypedTarget, buildPath, true);
+
+//            Debug.Log($"Build finished");
+//        }
+//    }
+//}
