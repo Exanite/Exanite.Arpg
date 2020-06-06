3a439729f6cf82451ef3db7453baab58841d7a18
Exanite <Exanite78012@gmail.com>
2020-06-06 00:43:10 -0700

    Moved most of the code out of the Source folder and moved them into Exanite.Arpg at the root of the Assets folder. This is so Visual Studio properly generates correct namespaces for files and keeps things more organized. Also added Assembly definitions to each sub-project.
    Former-commit-id: 8492b855a8be4e4fc2f9ef75b29932f24bc65013

---
+++ Assets/Exanite.Arpg/Modding/Editor/Modding/ModUtility.cs
@@ -0,0 +1,76 @@
+﻿//using System.IO;
+//using Exanite.Core.Extensions;
+//using UnityEditor;
+
+//namespace Exanite.Arpg.Modding.Editor
+//{
+//    public static class ModUtility
+//    {
+//        public const string DefaultModBuildPath = "Builds/Mods/";
+
+//        public static void SetUpModFolders(ModInfo mod)
+//        {
+//            if (!TryGetModPath(mod, out string modPath))
+//            {
+//                return;
+//            }
+
+//            if (string.IsNullOrWhiteSpace(modPath))
+//            {
+//                return;
+//            }
+
+//            DirectoryInfo sourceCodeDirectory = new DirectoryInfo(Path.Combine(modPath, ModInfo.SourceCodeFolderName));
+//            DirectoryInfo assetsDirectory = new DirectoryInfo(Path.Combine(modPath, ModInfo.AssetsFolderName));
+
+//            sourceCodeDirectory.Create();
+//            assetsDirectory.Create();
+
+//            AssetDatabase.Refresh();
+//        }
+
+//        public static void Build(ModInfo mod, string buildPath, bool overwrite = false)
+//        {
+//            DirectoryInfo buildDirectory = new DirectoryInfo(buildPath);
+
+//            if (!buildDirectory.IsEmpty() && overwrite)
+//            {
+//                foreach (var file in buildDirectory.EnumerateFiles())
+//                {
+//                    file.Delete();
+//                }
+
+//                foreach (var directory in buildDirectory.EnumerateDirectories())
+//                {
+//                    directory.Delete();
+//                }
+//            }
+//            else
+//            {
+//                return;
+//            }
+
+//            buildDirectory.Create();
+//        }
+
+//        public static bool TryGetModPath(ModInfo mod, out string path)
+//        {
+//            string assetPath = AssetDatabase.GetAssetPath(mod);
+
+//            if (string.IsNullOrWhiteSpace(assetPath))
+//            {
+//                path = null;
+//                return false;
+//            }
+
+//            path = Path.GetDirectoryName(assetPath);
+//            return true;
+//        }
+
+//        public static void CreateDefaultModBuildPath()
+//        {
+//            DirectoryInfo buildDirectory = new DirectoryInfo(DefaultModBuildPath);
+//            buildDirectory.Create();
+//        }
+//    }
+//}
