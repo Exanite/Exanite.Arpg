973c4e8e48c87af89ca67d349f90eb043a96773f
Exanite <Exanite78012@gmail.com>
2020-06-07 01:01:49 -0700

    Moved constants to a separate class.
    Former-commit-id: 004c23304b472163bbe9e452353c68c8ab11d917

---
+++ Assets/Exanite.Arpg/AssetManagement/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Exanite.Arpg.AssetManagement
+{
+    public static class Constants
+    {
+        public static readonly string PackageInfoFileExtension = "packageinfo";
+        public static readonly string AssetBundleFileExtension = "assetbundle";
+    }
+}
