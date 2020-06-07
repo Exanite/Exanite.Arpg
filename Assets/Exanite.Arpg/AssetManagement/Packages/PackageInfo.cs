0a4c45e9d292c62fd114ddca92be70da43fe3cb7
Exanite <Exanite78012@gmail.com>
2020-06-06 23:32:55 -0700

    Renamed AssetBundles namespace and related classes to Package to differentiate from Unity's AssetBundles. Moved the FileExtension constant to PackageBuilder. Might move later. PackageBuilder now adds the .assetbundle extension to built AssetBundles.
    Former-commit-id: bebdab2c36229803657adf30a29b4275d98d6691

---
+++ Assets/Exanite.Arpg/AssetManagement/Packages/PackageInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Exanite.Arpg.AssetManagement.Packages
+{
+    public class PackageInfo
+    {
+        private List<PackageAssetEntry> entries;
+
+        public PackageInfo()
+        {
+            Entries = new List<PackageAssetEntry>();
+        }
+
+        public PackageInfo(int capacity)
+        {
+            entries = new List<PackageAssetEntry>(capacity);
+        }
+
+        public List<PackageAssetEntry> Entries
+        {
+            get
+            {
+                return entries;
+            }
+
+            set
+            {
+                entries = value;
+            }
+        }
+    }
+}
