e5a8179a770d8001772d5f4d105afbfd1ec93c59
Exanite <Exanite78012@gmail.com>
2020-06-07 17:48:33 -0700

    Added the Package class and updated the AssetManagement asmdef to include Json.Net. PackageBuilder now properly overwrites existing assetbundle builds when building. Updated other miscellaneous files.

---
+++ Assets/Exanite.Arpg/AssetManagement/Packages/Package.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Exanite.Arpg.AssetManagement.Packages
+{
+    public class Package
+    {
+        private AssetBundle assetBundle;
+        private PackageInfo info;
+
+        public AssetBundle AssetBundle
+        {
+            get
+            {
+                return assetBundle;
+            }
+
+            set
+            {
+                assetBundle = value;
+            }
+        }
+
+        public PackageInfo Info
+        {
+            get
+            {
+                return info;
+            }
+
+            set
+            {
+                info = value;
+            }
+        }
+
+        public static Package LoadPackage(string packageInfoPath)
+        {
+            string assetBundlePath = Path.ChangeExtension(packageInfoPath, Constants.AssetBundleFileExtension);
+            var assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+
+            string packageInfoJson = File.ReadAllText(packageInfoPath);
+            var packageInfo = JsonConvert.DeserializeObject<PackageInfo>(packageInfoJson);
+
+            var package = new Package
+            {
+                AssetBundle = assetBundle,
+                Info = packageInfo,
+            };
+
+            return package;
+        }
+    }
+}
