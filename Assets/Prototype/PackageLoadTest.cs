3485d984ba2e09a7dc79f1fe8de1cbc215b5d566
Exanite <Exanite78012@gmail.com>
2020-06-11 21:22:11 -0700

    Package load test class.
    Former-commit-id: e5b36cf949c99c8ca5ff0e9d0ee9ccfd12e178de

---
+++ Assets/Prototype/PackageLoadTest.cs
@@ -0,0 +1,26 @@
+﻿using Exanite.Arpg.AssetManagement.Packages;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class PackageLoadTest : MonoBehaviour
+    {
+        public Package package;
+
+        private void Start()
+        {
+            package = Package.LoadPackage(@"D:\Repositories\Exanite.Arpg\Assets\Mods\ExampleMod\Bundles\examplemod.packageinfo");
+
+            Debug.Log(package.AssetBundle.name);
+
+            foreach (var item in package.Entries)
+            {
+                Debug.Log(item.Key);
+            }
+
+            var obj = package.AssetBundle.LoadAsset<Sprite>("ExampleMod/Sprites/Flasks/Health/Flask_1");
+
+            Debug.Log(obj);
+        }
+    }
+}
