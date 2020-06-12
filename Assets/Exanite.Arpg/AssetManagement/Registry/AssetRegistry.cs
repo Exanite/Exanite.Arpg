f729a1b07dbb263e5c475a78bbb6420ea3c3b7fa
Exanite <Exanite78012@gmail.com>
2020-06-11 21:21:49 -0700

    Added a crude method for loading Packages into the AssetRegistry.
    Former-commit-id: 3c1764d123e5d0d9341d7c61b84265736529adfa

--- Assets/Exanite.Arpg/AssetManagement/Registry/AssetRegistry.cs
+++ Assets/Exanite.Arpg/AssetManagement/Registry/AssetRegistry.cs
@@ -8,9 +8,30 @@ namespace Exanite.Arpg.AssetRegistry
         public Dictionary<Key, AssetEntry> entries;
         public Dictionary<Key, Package> packages;
 
-        public void AddEntry()
+        public void LoadPackage(Package package)
         {
+            packages.Add(package.Name, package);
 
+            Key key;
+            AssetEntry assetEntry;
+            foreach (var packageAssetEntry in package.Entries)
+            {
+                key = packageAssetEntry.Key;
+
+                if (entries.ContainsKey(key))
+                {
+                    assetEntry = entries[key];
+                }
+                else
+                {
+                    assetEntry = new AssetEntry();
+                    entries[key] = assetEntry;
+                }
+
+                assetEntry.Key = key;
+                assetEntry.Package = package;
+                assetEntry.Type = packageAssetEntry.Type;
+            }
         }
     }
 }
