61564a5a18029388527562c35c0162c4d4c1d983
Exanite <Exanite78012@gmail.com>
2020-06-11 21:21:12 -0700

    Added Pathfinding assembly and some classes related to editing and generating a NavGrid. No pathfinding yet.

---
+++ Assets/Exanite.Arpg/Pathfinding/Node.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Exanite.Arpg.Pathfinding
+{
+    public class Node
+    {
+        private Vector3 position;
+        private NodeType type = NodeType.Walkable;
+
+        private HashSet<Node> connectedNodes = new HashSet<Node>();
+
+        public Node() { }
+
+        public Node(Vector3 position, NodeType type)
+        {
+            Position = position;
+            Type = type;
+        }
+
+        public Vector3 Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                position = value;
+            }
+        }
+
+        public NodeType Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public IEnumerable<Node> ConnectedNodes
+        {
+            get
+            {
+                return connectedNodes;
+            }
+        }
+
+        public void AddConnection(Node node)
+        {
+            if (!connectedNodes.Contains(node) && node != this && node != null)
+            {
+                connectedNodes.Add(node);
+            }
+        }
+
+        public void RemoveConnection(Node node)
+        {
+            connectedNodes.Remove(node);
+        }
+    }
+}
