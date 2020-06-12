1fae61192c3dcba2afe4158b62c2389c9ea71a05
Exanite <Exanite78012@gmail.com>
2020-06-12 15:44:21 -0700

    UnityToSerilogLogHandler now has to be manually activated instead of automatically activating on instantiation and can also be deactivated as well. Added a LoggingLevelSwitch binding to DI so that the minimum logging level can be controlled elsewhere. Added documentation and some other general improvements.
    Former-commit-id: c8e9170192d041baf54715c1b9d7761cb441b4c4

--- Assets/Exanite.Arpg/Installers/LogInstaller.cs
+++ Assets/Exanite.Arpg/Installers/LogInstaller.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Exanite.Arpg.Logging;
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
 using Serilog.Formatting;
 using Serilog.Formatting.Display;
@@ -20,6 +21,7 @@ namespace Exanite.Arpg.Installers
     {
         [SerializeField] private bool logToFileInEditor = false;
         [SerializeField] private bool logToUnityConsole = true;
+        [SerializeField] private bool interceptUnityDebugLogMessages = true;
         [SerializeField] private bool includeTimestampInUnityConsole = false;
         [SerializeField] private string timestampFormat = "[{Timestamp:HH:mm:ss}]";
         [SerializeField] private string format = "[{Level}] [{ShortContext}]: {Message:lj}{NewLine}{Exception}";
@@ -54,6 +56,22 @@ namespace Exanite.Arpg.Installers
             set
             {
                 logToUnityConsole = value;
+            }
+        }
+
+        /// <summary>
+        /// Should the <see cref="Logger"/> intercept Unity Debug.Log messages?
+        /// </summary>
+        public bool InterceptUnityDebugLogMessages
+        {
+            get
+            {
+                return interceptUnityDebugLogMessages;
+            }
+
+            set
+            {
+                interceptUnityDebugLogMessages = value;
             }
         }
 
@@ -124,9 +142,24 @@ namespace Exanite.Arpg.Installers
         /// </summary>
         public override void InstallBindings()
         {
+            Container.Bind(typeof(LoggingLevelSwitch)).To<LoggingLevelSwitch>().FromMethod(CreateLevelSwitch).AsSingle();
+
             Container.Bind(typeof(ILogger), typeof(IDisposable)).To<Logger>().FromMethod(CreateLogger).AsSingle().NonLazy();
 
-            Container.Bind(typeof(UnityToSerilogLogHandler), typeof(IDisposable)).To<UnityToSerilogLogHandler>().AsSingle().NonLazy();
+            Container.Bind(typeof(UnityToSerilogLogHandler), typeof(IDisposable)).To<UnityToSerilogLogHandler>().FromMethod(CreateUnityToSerilogLogHandler).AsSingle().NonLazy();
+        }
+
+        /// <summary>
+        /// Creates a <see cref="LoggingLevelSwitch"/> for changing the minimum level of logged events
+        /// </summary>
+        private LoggingLevelSwitch CreateLevelSwitch(InjectContext ctx)
+        {
+            var levelSwitch = new LoggingLevelSwitch
+            {
+                MinimumLevel = MinimumLevel
+            };
+
+            return levelSwitch;
         }
 
         /// <summary>
@@ -134,14 +167,16 @@ namespace Exanite.Arpg.Installers
         /// </summary>
         private Logger CreateLogger(InjectContext ctx)
         {
+            var levelSwitch = ctx.Container.Resolve<LoggingLevelSwitch>();
+
             var config = new LoggerConfiguration()
                 .Enrich.WithProperty("SourceContext", "Default")
                 .Enrich.With<ShortContextEnricher>()
                 .Enrich.WithThreadId()
                 .Enrich.WithThreadName()
-                .MinimumLevel.Is(MinimumLevel);
+                .MinimumLevel.ControlledBy(levelSwitch);
 
-            if (Application.isEditor && LogToFileInEditor)
+            if (LogToFileInEditor || !Application.isEditor)
             {
                 string path = Path.GetFullPath(Path.Combine(Application.persistentDataPath, "Logs", $@"Log-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.log"));
 
@@ -158,6 +193,9 @@ namespace Exanite.Arpg.Installers
             return log;
         }
 
+        /// <summary>
+        /// Configures the logger to write to two files: one formatted as human-readable text, another as json
+        /// </summary>
         private void WriteToFile(LoggerConfiguration config, string path)
         {
             ITextFormatter fileFormatter = new MessageTemplateTextFormatter(string.Join(" ", TimestampFormat, Format));
@@ -166,6 +204,9 @@ namespace Exanite.Arpg.Installers
                 .WriteTo.File(new JsonFormatter(), $"{path}.json");
         }
 
+        /// <summary>
+        /// Configures the logger to write to the Unity Console
+        /// </summary>
         private void WriteToUnityConsole(LoggerConfiguration config)
         {
             ITextFormatter unityConsoleFormatter = new MessageTemplateTextFormatter(string.Join((IncludeTimestampInUnityConsole ? TimestampFormat : null), Format));
@@ -173,5 +214,19 @@ namespace Exanite.Arpg.Installers
             config.WriteTo.Sink(new UnityConsoleSink(Debug.unityLogger.logHandler, unityConsoleFormatter));
         }
 
+        /// <summary>
+        /// Creates a <see cref="UnityToSerilogLogHandler"/> and activates it if <see cref="InterceptUnityDebugLogMessages"/> is <see langword="true"/>
+        /// </summary>
+        private UnityToSerilogLogHandler CreateUnityToSerilogLogHandler(InjectContext ctx)
+        {
+            var handler = ctx.Container.Instantiate<UnityToSerilogLogHandler>();
+
+            if (InterceptUnityDebugLogMessages)
+            {
+                handler.Activate();
+            }
+
+            return handler;
+        }
     }
 }
