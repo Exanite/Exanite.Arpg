f22c0bb4b2f08a39b83cbd252577af4cca8068ae
Exanite <Exanite78012@gmail.com>
2020-06-11 21:21:12 -0700

    Added Pathfinding assembly and some classes related to editing and generating a NavGrid. No pathfinding yet.
    Former-commit-id: 61564a5a18029388527562c35c0162c4d4c1d983

---
+++ Assets/Exanite.Arpg/Pathfinding/NavGridPainter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Exanite.Arpg.Pathfinding
+{
+    public class NavGridPainter : MonoBehaviour
+    {
+        public NavGrid target;
+
+        private Coroutine currentTask;
+
+        private void Update()
+        {
+            if (target.nodes != null)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    currentTask = StartCoroutine(PaintGrid());
+                }
+                else if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    StopCoroutine(currentTask);
+
+                    currentTask = null;
+                }
+            }
+        }
+
+        private IEnumerator PaintGrid()
+        {
+            HashSet<Node> paintedNodes = new HashSet<Node>();
+            NodeType paintAction = NodeType.Walkable;
+            bool firstRun = true;
+
+            while (true)
+            {
+                if (target.RaycastNode(Camera.main.ScreenPointToRay(Input.mousePosition), out Node node))
+                {
+                    if (node != null && !paintedNodes.Contains(node))
+                    {
+                        if (firstRun == true)
+                        {
+                            switch (node.Type)
+                            {
+                                case NodeType.NonWalkable:
+                                {
+                                    paintAction = NodeType.Walkable;
+                                    break;
+                                }
+
+                                case NodeType.Walkable:
+                                {
+                                    paintAction = NodeType.NonWalkable;
+                                    break;
+                                }
+                            }
+
+                            firstRun = false;
+                        }
+
+                        node.Type = paintAction;
+
+                        paintedNodes.Add(node);
+                    }
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Exanite.Arpg/Pathfinding/NodeType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exanite.Arpg.Pathfinding
+{
+    [Serializable]
+    public enum NodeType
+    {
+        Walkable,
+        NonWalkable,
+    }
+}
