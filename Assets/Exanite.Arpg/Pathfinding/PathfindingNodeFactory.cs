510fcea3288e0bd1bcad2271e7eb9440ca5f08da
Exanite <Exanite78012@gmail.com>
2020-06-13 18:04:35 -0700

    Uncommented out PathfindingNode and moved factory related members into a separate class.

---
+++ Assets/Exanite.Arpg/Pathfinding/PathfindingNodeFactory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Exanite.Arpg.Pathfinding.Graphs;
+
+namespace Exanite.Arpg.Pathfinding
+{
+    public class PathfindingNodeFactory
+    {
+        private readonly Dictionary<Node, PathfindingNode> activeInstances = new Dictionary<Node, PathfindingNode>();
+
+        private static readonly Stack<PathfindingNode> instancePool = new Stack<PathfindingNode>();
+
+        public PathfindingNode GetFor(Node node)
+        {
+            if (activeInstances.ContainsKey(node))
+            {
+                return GetExistingFor(node);
+            }
+            else
+            {
+                return GetNewFor(node);
+            }
+        }
+
+        private PathfindingNode GetExistingFor(Node node)
+        {
+            return activeInstances[node];
+        }
+
+        private PathfindingNode GetNewFor(Node node)
+        {
+            PathfindingNode pathfindingNode;
+
+            if (instancePool.Count > 0)
+            {
+                pathfindingNode = instancePool.Pop();
+            }
+            else
+            {
+                pathfindingNode = new PathfindingNode();
+            }
+
+            pathfindingNode.Node = node;
+
+            activeInstances.Add(node, pathfindingNode);
+
+            return pathfindingNode;
+        }
+
+        public void Release(PathfindingNode pathfindingNode)
+        {
+            activeInstances.Remove(pathfindingNode.Node);
+
+            pathfindingNode.Reset();
+
+            instancePool.Push(pathfindingNode);
+        }
+    }
+}
