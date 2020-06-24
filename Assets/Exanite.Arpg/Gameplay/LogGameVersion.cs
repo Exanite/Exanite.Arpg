38e8a4fc181b36e30c30a30a5263e60de531719f
Exanite <42710136+Exanite@users.noreply.github.com>
2020-06-24 02:25:25 -0700

    Add documentation for LogGameVersion

--- Assets/Exanite.Arpg/Gameplay/LogGameVersion.cs
+++ Assets/Exanite.Arpg/Gameplay/LogGameVersion.cs
@@ -4,6 +4,9 @@ using ILogger = Serilog.ILogger;
 
 namespace Exanite.Arpg.Gameplay
 {
+    /// <summary>
+    /// Used to log the current game version
+    /// </summary>
     public class LogGameVersion : MonoBehaviour
     {
         private ILogger log;
@@ -16,7 +19,10 @@ namespace Exanite.Arpg.Gameplay
             LogCurrentVersion();
         }
 
-        private void LogCurrentVersion()
+        /// <summary>
+        /// Logs the current game version
+        /// </summary>
+        public void LogCurrentVersion()
         {
             log.Information("The current version of the game is {Version}", Application.version);
         }
