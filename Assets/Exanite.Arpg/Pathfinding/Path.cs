b33b3d9ee3cf3b95795bf1a65c8a1cc1a1ebdb72
Exanite <42710136+Exanite@users.noreply.github.com>
2020-06-25 19:00:20 -0700

    Refactor Pathfinder and Path
    Add documentation to Path.
    Add a second constructor to Path that takes in a list of Nodes.
    Improve end portion of Pathfinder.FindPath.
    
    
    Former-commit-id: 6a49dad8f4fc0908d54b8f64f28ad84e580257b4

--- Assets/Exanite.Arpg/Pathfinding/Path.cs
+++ Assets/Exanite.Arpg/Pathfinding/Path.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using Exanite.Arpg.Pathfinding.Graphs;
 using UnityEngine;
 
 namespace Exanite.Arpg.Pathfinding
 {
+    /// <summary>
+    /// Represents a path defined by a collection of waypoints
+    /// </summary>
     public class Path
     {
         private List<Vector3> waypoints;
 
+        /// <summary>
+        /// Creates a new <see cref="Path"/> using a List of waypoints
+        /// </summary>
         public Path(List<Vector3> waypoints)
         {
             Waypoints = waypoints ?? throw new ArgumentNullException(nameof(waypoints));
         }
 
+        /// <summary>
+        /// Creates a new <see cref="Path"/> using a List of <see cref="Node"/>s
+        /// </summary>
+        public Path(List<Node> nodes)
+        {
+            waypoints = new List<Vector3>(nodes.Count);
+
+            foreach (var node in nodes)
+            {
+                waypoints.Add(node.Position);
+            }
+        }
+
+        /// <summary>
+        /// Waypoints contained in this <see cref="Path"/>
+        /// </summary>
         public List<Vector3> Waypoints
         {
             get
@@ -20,7 +43,7 @@ namespace Exanite.Arpg.Pathfinding
                 return waypoints;
             }
 
-            set
+            private set
             {
                 waypoints = value;
             }
