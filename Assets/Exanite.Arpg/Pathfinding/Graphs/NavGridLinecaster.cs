005217bbf33a839090a74178c081a1b56111912e
Exanite <42710136+Exanite@users.noreply.github.com>
2020-06-28 08:18:01 -0700

    Add NavGridLinecaster.cs
    This is a temporary class for implementing and testing a method that checks if there is a direct path between two nodes.
    The IsDirectlyWalkable method will be used later for the implementation of the Theta* algorithm.
    
    
    Former-commit-id: 1b08921dc00d23e4ed7db744898e59dbce923c19

---
+++ Assets/Exanite.Arpg/Pathfinding/Graphs/NavGridLinecaster.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Exanite.Arpg.Pathfinding.Graphs
+{
+    public class NavGridLinecaster : MonoBehaviour
+    {
+        public NavGrid target;
+
+        public KeyCode setAKey = KeyCode.A;
+        public KeyCode setBKey = KeyCode.B;
+
+        private Node nodeA;
+        private Node nodeB;
+
+        private void Update()
+        {
+            if (!target)
+            {
+                return;
+            }
+
+            if (Input.GetKey(setAKey))
+            {
+                if (target.RaycastNode(Camera.main.ScreenPointToRay(Input.mousePosition), out Node node))
+                {
+                    nodeA = node;
+                }
+            }
+
+            if (Input.GetKey(setBKey))
+            {
+                if (target.RaycastNode(Camera.main.ScreenPointToRay(Input.mousePosition), out Node node))
+                {
+                    nodeB = node;
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (nodeA != null && nodeB != null)
+            {
+                Gizmos.color = IsDirectlyWalkable(nodeA, nodeB) ? Color.green : Color.red;
+
+                Gizmos.DrawLine(nodeA.Position, nodeB.Position);
+            }
+        }
+
+        public bool IsDirectlyWalkable(Node a, Node b)
+        {
+            return false;
+        }
+    }
+}
