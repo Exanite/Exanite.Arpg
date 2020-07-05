92d4627180332246b54328cb4a0e7d8ea3164596
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-05 16:59:31 -0700

    Add game version logging on game start
    This reimplements the changes made in pull request #9
    since that pull request has been broken due to the repo's Git history
    being rewritten to remove Git LFS.
    
    Changes:
    Add LogGameVersion.cs.
    Set project settings version to 'dev'.
    Add LogGameVersion component to ProjectContext.

---
+++ Assets/Exanite.Arpg/Gameplay/LogGameVersion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Zenject;
+using ILogger = Serilog.ILogger;
+
+namespace Exanite.Arpg.Gameplay
+{
+    /// <summary>
+    /// Used to log the current game version
+    /// </summary>
+    public class LogGameVersion : MonoBehaviour
+    {
+        private ILogger log;
+
+        [Inject]
+        public void Inject(ILogger log)
+        {
+            this.log = log.ForContext<LogGameVersion>();
+
+            LogCurrentVersion();
+        }
+
+        /// <summary>
+        /// Logs the current game version
+        /// </summary>
+        public void LogCurrentVersion()
+        {
+            log.Information("The current version of the game is '{Version}'", Application.version);
+        }
+    }
+}
