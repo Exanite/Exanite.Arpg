2fb964108b34d4f07c9dd0621596fdbe9da68e4f
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-08 08:54:38 -0700

    Add SerilogLogAdapter

---
+++ Assets/Exanite.Arpg/Logging/SerilogLogAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace Exanite.Arpg.Logging
+{
+    public class SerilogLogAdapter : ILog
+    {
+        private readonly ILogger inner;
+
+        public SerilogLogAdapter(ILogger inner)
+        {
+            this.inner = inner;
+        }   
+
+        public ILog ForContext(string property, object value)
+        {
+            return new SerilogLogAdapter(inner.ForContext(property, value));
+        }
+
+        public bool IsEnabled(LogLevel level)
+        {
+            return inner.IsEnabled((LogEventLevel)level);
+        }
+
+        public void Log(LogEntry entry)
+        {
+            inner.Write((LogEventLevel)entry.Level, entry.Exception, entry.MessageTemplate, entry.PropertyValues);
+        }
+    }
+}
