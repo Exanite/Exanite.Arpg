a43a8e0a9c84e0aec8bef1639a19a7ad208f7c92
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-10 21:44:07 -0700

    Add PhysicsSceneInstaller
    The installer also has an option to require the scene to be loaded with a local PhysicsScene.

---
+++ Assets/Prototype/DarkRift/Shared/PhysicsSceneInstaller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Exanite.Arpg.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class PhysicsSceneInstaller : MonoInstaller
+{
+    [SerializeField] private bool requireLocalPhysicsScene = true;
+
+    public bool RequireLocalPhysicsScene
+    {
+        get
+        {
+            return requireLocalPhysicsScene;
+        }
+
+        set
+        {
+            requireLocalPhysicsScene = value;
+        }
+    }
+
+    public override void InstallBindings()
+    {
+        Container.Bind<Scene>().FromMethod(GetScene).AsSingle().NonLazy();
+        Container.Bind<PhysicsScene>().FromMethod(GetPhysicsScene).AsSingle().NonLazy();
+    }
+
+    private Scene GetScene()
+    {
+        return gameObject.scene;
+    }
+
+    private PhysicsScene GetPhysicsScene()
+    {
+        var physicsScene = gameObject.scene.GetPhysicsScene();
+
+        if (RequireLocalPhysicsScene && physicsScene == Physics.defaultPhysicsScene)
+        {
+            throw new InvalidOperationException($"Scene PhysicsScene is same as global. Make sure this scene is not loaded with the option 'LocalPhysicsMode.None'.");
+        }
+
+        return physicsScene;
+    }
+}
