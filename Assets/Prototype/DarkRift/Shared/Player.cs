b218b60200ff863d4f4844a29aa5bc182080ce30
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-11 20:15:53 -0700

    Update Player to hold a player's id, transform, and input

--- Assets/Prototype/DarkRift/Shared/Player.cs
+++ Assets/Prototype/DarkRift/Shared/Player.cs
@@ -1,4 +1,21 @@
-﻿public class Player
-{
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
+namespace Prototype.DarkRift.Shared
+{
+    public class Player
+    {
+        public ushort id;
+        public Transform transform;
+
+        public Vector2 movementInput;
+
+        public Player(ushort id, Scene scene)
+        {
+            this.id = id;
+
+            transform = new GameObject($"Player {id.ToString()}").transform;
+            SceneManager.MoveGameObjectToScene(transform.gameObject, scene);
+        }
+    } 
 }
