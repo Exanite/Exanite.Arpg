9f57360e46384958ddb11ecb128e45240ad950c5
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-19 18:27:35 -0700

    Add PlayerConnection and PlayerManager

---
+++ Assets/Exanite.Arpg/Networking/Server/PlayerConnection.cs
@@ -0,0 +1,12 @@
+﻿using DarkRift.Server;
+
+namespace Exanite.Arpg.Networking.Server
+{
+    public class PlayerConnection
+    {
+        public ushort id;
+        public IClient client;
+
+        public string name;
+    } 
+}
---
+++ Assets/Exanite.Arpg/Networking/Server/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Exanite.Arpg.Networking.Server
+{
+    public class PlayerManager
+    {
+        private Dictionary<ushort, PlayerConnection> playersByID = new Dictionary<ushort, PlayerConnection>();
+        private Dictionary<string, PlayerConnection> playersByName = new Dictionary<string, PlayerConnection>();
+
+        public void AddPlayer(PlayerConnection connection)
+        {
+            playersByID.Add(connection.id, connection);
+            playersByName.Add(connection.name, connection);
+        }
+
+        public void RemovePlayer(PlayerConnection connection)
+        {
+            playersByID.Remove(connection.id);
+            playersByName.Remove(connection.name);
+        }
+
+        public bool Contains(ushort id)
+        {
+            return playersByID.ContainsKey(id);
+        }
+
+        public bool Contains(string name)
+        {
+            return playersByName.ContainsKey(name);
+        }
+    }
+}
