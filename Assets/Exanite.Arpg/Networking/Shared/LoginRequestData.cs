037e81838c1f5d590f597fa8d6071f98aa1f6b22
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-19 18:30:28 -0700

    Add LoginRequestData

---
+++ Assets/Exanite.Arpg/Networking/Shared/LoginRequestData.cs
@@ -0,0 +1,57 @@
+﻿using DarkRift;
+
+namespace Exanite.Arpg.Networking.Shared
+{
+    /// <summary>
+    /// Data sent during a <see cref="MessageTag.LoginRequest"/>
+    /// </summary>
+    public class LoginRequestData : IDarkRiftSerializable
+    {
+        private string playerName;
+        private string gameVersion;
+
+        /// <summary>
+        /// Name of the player that is requesting the connection
+        /// </summary>
+        public string PlayerName
+        {
+            get
+            {
+                return playerName;
+            }
+
+            set
+            {
+                playerName = value;
+            }
+        }
+
+        /// <summary>
+        /// Game version the player's client is using
+        /// </summary>
+        public string GameVersion
+        {
+            get
+            {
+                return gameVersion;
+            }
+
+            set
+            {
+                gameVersion = value;
+            }
+        }
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            playerName = e.Reader.ReadString();
+            gameVersion = e.Reader.ReadString();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(PlayerName);
+            e.Writer.Write(GameVersion);
+        }
+    }
+}
