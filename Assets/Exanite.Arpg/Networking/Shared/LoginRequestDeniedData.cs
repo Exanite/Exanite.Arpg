f568956c0a5563e7c84f1e5d0aab73ee59917b5d
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-19 19:23:05 -0700

    Add LoginRequestDeniedData

---
+++ Assets/Exanite.Arpg/Networking/Shared/LoginRequestDeniedData.cs
@@ -0,0 +1,38 @@
+﻿using DarkRift;
+
+namespace Exanite.Arpg.Networking.Shared
+{
+    /// <summary>
+    /// Data sent during a <see cref="MessageTag.LoginRequestDenied"/> message
+    /// </summary>
+    public class LoginRequestDeniedData : IDarkRiftSerializable
+    {
+        private string reason;
+
+        /// <summary>
+        /// The reason for the disconnection
+        /// </summary>
+        public string Reason
+        {
+            get
+            {
+                return reason;
+            }
+
+            set
+            {
+                reason = value;
+            }
+        }
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            Reason = e.Reader.ReadString();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(Reason);
+        }
+    }
+}
