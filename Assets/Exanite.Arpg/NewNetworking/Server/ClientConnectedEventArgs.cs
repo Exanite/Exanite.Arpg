f61b7fc017fea0d1ab7f6d16289ff27b29054d5b
Exanite <42710136+Exanite@users.noreply.github.com>
2020-07-28 12:29:35 -0700

    Add ClientConnected/Disconnected event args

---
+++ Assets/Exanite.Arpg/NewNetworking/Server/ClientConnectedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LiteNetLib;
+
+namespace Exanite.Arpg.NewNetworking.Server
+{
+    public class ClientConnectedEventArgs : EventArgs
+    {
+        private NetPeer peer;
+
+        public ClientConnectedEventArgs(NetPeer peer)
+        {
+            Peer = peer;
+        }
+
+        public NetPeer Peer
+        {
+            get
+            {
+                return peer;
+            }
+
+            set
+            {
+                peer = value;
+            }
+        }
+    }
+}
---
+++ Assets/Exanite.Arpg/NewNetworking/Server/ClientDisconnectedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LiteNetLib;
+
+namespace Exanite.Arpg.NewNetworking.Server
+{
+    public class ClientDisconnectedEventArgs : EventArgs
+    {
+        private NetPeer peer;
+
+        public ClientDisconnectedEventArgs(NetPeer peer)
+        {
+            Peer = peer;
+        }
+
+        public NetPeer Peer
+        {
+            get
+            {
+                return peer;
+            }
+
+            set
+            {
+                peer = value;
+            }
+        }
+    }
+}
