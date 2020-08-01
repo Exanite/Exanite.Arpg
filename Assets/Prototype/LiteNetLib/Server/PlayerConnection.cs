a450b44669137e9ed0f226ad370e99cd7ca76774
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-01 01:14:20 -0700

    Add PlayerConnection and PlayerManager

---
+++ Assets/Prototype/LiteNetLib/Server/PlayerConnection.cs
@@ -0,0 +1,63 @@
+﻿using LiteNetLib;
+
+namespace Prototype.LiteNetLib.Server
+{
+    /// <summary>
+    /// Represents a player connected to the server
+    /// </summary>
+    public class PlayerConnection
+    {
+        private int id;
+        private NetPeer client;
+
+        private string name;
+
+        /// <summary>
+        /// Network ID of the connected player
+        /// </summary>
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="NetPeer"/> of the connected player
+        /// </summary>
+        public NetPeer Client
+        {
+            get
+            {
+                return client;
+            }
+
+            set
+            {
+                client = value;
+            }
+        }
+
+        /// <summary>
+        /// The name of the connected player
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
---
+++ Assets/Prototype/LiteNetLib/Server/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Prototype.LiteNetLib.Server
+{
+    public class PlayerManager
+    {
+        private Dictionary<int, PlayerConnection> playersByID = new Dictionary<int, PlayerConnection>();
+
+        public ICollection<PlayerConnection> ConnectedPlayers
+        {
+            get
+            {
+                return playersByID.Values;
+            }
+        }
+
+        public void AddPlayer(PlayerConnection connection)
+        {
+            playersByID.Add(connection.ID, connection);
+        }
+
+        public void RemovePlayer(PlayerConnection connection)
+        {
+            playersByID.Remove(connection.ID);
+        }
+
+        public bool Contains(ushort id)
+        {
+            return playersByID.ContainsKey(id);
+        }
+
+        public PlayerConnection GetPlayerConnection(ushort id)
+        {
+            return playersByID[id];
+        }
+    }
+}
