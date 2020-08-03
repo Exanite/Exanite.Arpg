f5fe85dbb7a1ccceb5b4448d15e4ccd72e35ff21
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-03 16:41:27 -0700

    Add events to PlayerManager

--- Assets/Prototype/LiteNetLib/Players/PlayerManager.cs
+++ Assets/Prototype/LiteNetLib/Players/PlayerManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using Exanite.Arpg.Networking;
 
 namespace Prototype.LiteNetLib.Players
 {
     public class PlayerManager
     {
         private Dictionary<int, Player> playersById = new Dictionary<int, Player>();
+
+        public event EventHandler<PlayerManager, Player> PlayerAddedEvent;
+        public event EventHandler<PlayerManager, Player> PlayerRemovedEvent;
 
         public ICollection<Player> Players
         {
@@ -17,16 +21,23 @@ namespace Prototype.LiteNetLib.Players
         public void AddPlayer(Player player)
         {
             playersById.Add(player.Id, player);
+
+            PlayerAddedEvent?.Invoke(this, player);
         }
 
         public void RemovePlayer(Player player)
         {
-            playersById.Remove(player.Id);
+            RemovePlayer(player.Id);
         }
 
         public void RemovePlayer(int id)
         {
-            playersById.Remove(id);
+            if (playersById.TryGetValue(id, out Player player))
+            {
+                playersById.Remove(id);
+
+                PlayerRemovedEvent?.Invoke(this, player);
+            }
         }
 
         public bool Contains(int id)
