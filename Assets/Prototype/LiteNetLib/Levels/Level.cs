7248e914d61e9f72d16c6336f5d9c247a0ee67de
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-02 18:57:39 -0700

    Add Level class

---
+++ Assets/Prototype/LiteNetLib/Levels/Level.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Exanite.Arpg;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Prototype.LiteNetLib.Levels
+{
+    public class Level
+    {
+        public Guid guid;
+        public Transform root;
+
+        private Scene scene;
+
+        public Level(GameObject levelPrefab)
+        {
+            guid = Guid.NewGuid();
+
+            var createSceneParameters = new CreateSceneParameters(LocalPhysicsMode.Physics3D);
+            scene = SceneManager.CreateScene(guid.ToString(), createSceneParameters);
+
+            root = CreateLevel(levelPrefab);
+        }
+
+        private Transform CreateLevel(GameObject levelPrefab)
+        {
+            var levelGameObject = scene.Instantiate(levelPrefab, Vector3.zero, Quaternion.identity);
+
+            return levelGameObject.transform;
+        }
+    }
+}
