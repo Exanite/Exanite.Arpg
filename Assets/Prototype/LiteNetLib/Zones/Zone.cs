71207bd77099d5ad5993e55e73fbb30b25343586
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-03 18:26:36 -0700

    Rename Levels to Zones
    This is to prevent conflicts with player levels later on.

--- Assets/Prototype/LiteNetLib/Zones/Zone.cs
+++ Assets/Prototype/LiteNetLib/Zones/Zone.cs
@@ -3,26 +3,26 @@ using Exanite.Arpg;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Prototype.LiteNetLib.Levels
+namespace Prototype.LiteNetLib.Zones
 {
-    public class Level
+    public class Zone
     {
         public Guid guid;
         public Transform root;
 
         private Scene scene;
 
-        public Level(GameObject levelPrefab)
+        public Zone(GameObject zonePrefab)
         {
             guid = Guid.NewGuid();
 
             var createSceneParameters = new CreateSceneParameters(LocalPhysicsMode.Physics3D);
             scene = SceneManager.CreateScene(guid.ToString(), createSceneParameters);
 
-            root = CreateLevel(levelPrefab);
+            root = CreateZone(zonePrefab);
         }
 
-        private Transform CreateLevel(GameObject levelPrefab)
+        private Transform CreateZone(GameObject levelPrefab)
         {
             var levelGameObject = scene.Instantiate(levelPrefab, Vector3.zero, Quaternion.identity);
 
