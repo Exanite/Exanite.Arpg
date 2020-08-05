4fe76f2e13225b297a2118351518a852cdc6da1e
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-04 19:00:06 -0700

    Add ZonePlayerEnter/Leave packets

---
+++ Assets/Prototype/Networking/Zones/Packets/ZonePlayerLeavePacket.cs
@@ -0,0 +1,20 @@
+﻿using Exanite.Arpg.Networking;
+using LiteNetLib.Utils;
+
+namespace Prototype.Networking.Zones.Packets
+{
+    public class ZonePlayerLeavePacket : IPacket
+    {
+        public int playerId;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            playerId = reader.GetInt();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(playerId);
+        }
+    }
+}
