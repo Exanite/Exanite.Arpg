d203641686845144650002570bc1a0464d8f2b5a
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-04 18:14:04 -0700

    Add ZoneManager

---
+++ Assets/Prototype/LiteNetLib/Zones/ZoneManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype.LiteNetLib.Zones
+{
+    public class ZoneManager : MonoBehaviour
+    {
+        public Dictionary<Guid, Zone> zones = new Dictionary<Guid, Zone>();
+
+        // ? Packet handlers here
+    }
+}
