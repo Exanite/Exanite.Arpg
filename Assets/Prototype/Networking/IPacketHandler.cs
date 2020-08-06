30bec5b4e3f5bc9fb70bba0bd32ceaa60cdadee5
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-05 18:11:32 -0700

    Add IPacketHandler

---
+++ Assets/Prototype/Networking/IPacketHandler.cs
@@ -0,0 +1,11 @@
+﻿using Exanite.Arpg.Networking;
+
+namespace Prototype.Networking
+{
+    public interface IPacketHandler
+    {
+        void RegisterPackets(UnityNetBase netBase);
+
+        void UnregisterPackets(UnityNetBase netBase);
+    }
+}
