cb1523d947f43a36f8315eb7023ea82566cdc5db
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-13 19:14:31 -0700

    Change ServerPlayerManager.CreateFor to return the newly created player

--- Assets/Prototype/Networking/Players/ServerPlayerManager.cs
+++ Assets/Prototype/Networking/Players/ServerPlayerManager.cs
@@ -28,12 +28,14 @@ namespace Prototype.Networking.Players
             }
         }
 
-        public void CreateFor(NetPeer peer) // add PlayerCreateSettings parameter
+        public ServerPlayer CreateFor(NetPeer peer) // add PlayerCreateSettings parameter
         {
             var player = new ServerPlayer(new PlayerConnection(peer));
 
             playersById.Add(player.Id, player);
             PlayerAddedEvent?.Invoke(this, player);
+
+            return player;
         }
 
         public void RemoveFor(NetPeer peer)
