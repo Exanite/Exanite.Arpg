fffe012c4a16c623e1bc18fcdffb7d841f1260c0
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-18 16:14:39 -0700

    Add MenuItem defines for GameBuilder methods

---
+++ Assets/Exanite.Arpg/Editor/Builds/MenuItemDefines.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Exanite.Arpg.Editor.Builds
+{
+    public static class MenuItemDefines
+    {
+        [MenuItem("Build/Build Client", priority = 0)]
+        public static void BuildClient()
+        {
+            GameBuilder.BuildClient();
+        }
+
+        [MenuItem("Build/Build Server", priority = 1)]
+        public static void BuildServer()
+        {
+            GameBuilder.BuildServer();
+        }
+
+        [MenuItem("Build/Build Client and Server", priority = 2)]
+        public static void BuildClientAndServer()
+        {
+            GameBuilder.BuildClient();
+            GameBuilder.BuildServer();
+        }
+
+        [MenuItem("Build/Open Builds folder", priority = 100)]
+        public static void OpenBuildsFolder()
+        {
+            Application.OpenURL(GameBuilder.BuildPath);
+        }
+    }
+}
