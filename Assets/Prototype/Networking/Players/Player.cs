90d916d9f288b84c2ae54c225f4430fe4772a413
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-17 22:59:09 -0700

    Add documentation to Player

--- Assets/Prototype/Networking/Players/Player.cs
+++ Assets/Prototype/Networking/Players/Player.cs
@@ -4,6 +4,9 @@ using Prototype.Networking.Zones;
 
 namespace Prototype.Networking.Players
 {
+    /// <summary>
+    /// Represents a player in the game
+    /// </summary>
     public class Player
     {
         private PlayerCharacter character;
@@ -11,12 +14,18 @@ namespace Prototype.Networking.Players
         private readonly int id;
         private readonly ZoneManager zoneManager;
 
+        /// <summary>
+        /// Creates a new <see cref="Player"/>
+        /// </summary>
         public Player(int id, ZoneManager zoneManager)
         {
             this.id = id;
             this.zoneManager = zoneManager;
         }
 
+        /// <summary>
+        /// The Id of the player
+        /// </summary>
         public int Id
         {
             get
@@ -25,6 +34,9 @@ namespace Prototype.Networking.Players
             }
         }
 
+        /// <summary>
+        /// The <see cref="Zone"/> the <see cref="Player"/> is currently in
+        /// </summary>
         public Zone CurrentZone
         {
             get
@@ -33,6 +45,9 @@ namespace Prototype.Networking.Players
             }
         }
 
+        /// <summary>
+        /// Is the <see cref="Player"/> currently loading a <see cref="Zone"/>?
+        /// </summary>
         public bool IsLoadingZone
         {
             get
@@ -41,6 +56,10 @@ namespace Prototype.Networking.Players
             }
         }
 
+        /// <summary>
+        /// The <see cref="PlayerCharacter"/> of the <see cref="Player"/><para/>
+        /// Note: <see langword="null"/> if the <see cref="Player"/> is not in a <see cref="Zone"/>
+        /// </summary>
         public PlayerCharacter Character
         {
             get
@@ -54,6 +73,9 @@ namespace Prototype.Networking.Players
             }
         }
 
+        /// <summary>
+        /// Creates a new <see cref="PlayerCharacter"/> in the <see cref="Player"/>'s current zone
+        /// </summary>
         public void CreatePlayerCharacter()
         {
             if (CurrentZone == null)
