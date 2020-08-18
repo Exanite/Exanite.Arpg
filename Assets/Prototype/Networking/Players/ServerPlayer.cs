703a29226a1a101171a18b0bc2d7e28ba22f316f
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-17 22:42:54 -0700

    Update documentation for ServerPlayer and PlayerConnection

--- Assets/Prototype/Networking/Players/ServerPlayer.cs
+++ Assets/Prototype/Networking/Players/ServerPlayer.cs
@@ -2,16 +2,25 @@
 
 namespace Prototype.Networking.Players
 {
+    /// <summary>
+    /// Represents a player connected to the server
+    /// </summary>
     public class ServerPlayer : Player
     {
         private readonly PlayerConnection connection;
         private readonly ServerZoneManager zoneManager;
 
+        /// <summary>
+        /// Creates a new <see cref="ServerPlayer"/>
+        /// </summary>
         public ServerPlayer(PlayerConnection connection, ServerZoneManager zoneManager) : base(connection.Id, zoneManager)
         {
             this.connection = connection;
         }
 
+        /// <summary>
+        /// The <see cref="Player"/>'s connection
+        /// </summary>
         public PlayerConnection Connection
         {
             get
