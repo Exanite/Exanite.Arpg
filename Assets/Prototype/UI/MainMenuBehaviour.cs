1337da82e874a814b6afdc29ead3afcafb8f3755
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-27 17:24:10 -0700

    Add namespace to MainMenuBehaviour

--- Assets/Prototype/UI/MainMenuBehaviour.cs
+++ Assets/Prototype/UI/MainMenuBehaviour.cs
@@ -5,141 +5,144 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Zenject;
 
-public class MainMenuBehaviour : MonoBehaviour
+namespace Prototype.UI
 {
-    public string username = "Player";
-    public string address = "127.0.0.1";
-    public string port = "17175";
-
-    private UIDocument document;
-    private VisualElement root;
-    private EventSystem eventSystem;
-
-    private ILog log;
-    private GameVersion gameVersion;
-    private SceneLoader sceneLoader;
-
-    [Inject]
-    public void Inject(ILog log, GameVersion gameVersion, SceneLoader sceneLoader)
+    public class MainMenuBehaviour : MonoBehaviour
     {
-        this.log = log;
-        this.gameVersion = gameVersion;
-        this.sceneLoader = sceneLoader;
-    }
+        public string username = "Player";
+        public string address = "127.0.0.1";
+        public string port = "17175";
 
-    private void Start()
-    {
-        document = GetComponent<UIDocument>();
-        root = document.rootVisualElement;
+        private UIDocument document;
+        private VisualElement root;
+        private EventSystem eventSystem;
 
-        DisplayVersion();
+        private ILog log;
+        private GameVersion gameVersion;
+        private SceneLoader sceneLoader;
 
-        RegisterTextFields();
-        RegisterButtons();
-    }
-
-    private void DisplayVersion()
-    {
-        root.Query<Label>("version").ForEach((label) =>
+        [Inject]
+        public void Inject(ILog log, GameVersion gameVersion, SceneLoader sceneLoader)
         {
-            label.text = $"{gameVersion.Company}.{gameVersion.Product} {gameVersion.Version}";
-        });
-    }
-
-    private void RegisterTextFields()
-    {
-        root.Query<TextField>().ForEach((field) =>
-        {
-            switch (field.name)
-            {
-                case "username-field":
-                    field.value = username;
-                    field.RegisterValueChangedCallback(OnUsernameFieldChanged); break;
-
-                case "address-field":
-                    field.value = address;
-                    field.RegisterValueChangedCallback(OnAddressFieldChanged); break;
-
-                case "port-field":
-                    field.value = port;
-                    field.RegisterValueChangedCallback(OnPortFieldChanged); break;
-
-                default: break;
-            }
-        });
-    }
-
-    private void RegisterButtons()
-    {
-        root.Query<Button>().ForEach((button) =>
-        {
-            switch (button.name)
-            {
-                case "start-client-button":
-                    button.RegisterCallback<ClickEvent>(OnStartClientButtonActivated); break;
-
-                case "start-server-button":
-                    button.RegisterCallback<ClickEvent>(OnStartServerButtonActivated); break;
-
-                case "start-client-server-button":
-                    button.RegisterCallback<ClickEvent>(OnStartClientServerButtonActivated); break;
-
-                default: break;
-            }
-        });
-    }
-
-    private void OnUsernameFieldChanged(ChangeEvent<string> e)
-    {
-        username = e.newValue;
-
-        root.Query<TextField>("username-field").ForEach((field) =>
-        {
-            field.SetValueWithoutNotify(username);
-        });
-    }
-
-    private void OnAddressFieldChanged(ChangeEvent<string> e)
-    {
-        address = e.newValue;
-
-        root.Query<TextField>("address-field").ForEach((field) =>
-        {
-            field.SetValueWithoutNotify(address);
-        });
-    }
-
-    private void OnPortFieldChanged(ChangeEvent<string> e)
-    {
-        port = e.newValue;
-
-        root.Query<TextField>("port-field").ForEach((field) =>
-        {
-            field.SetValueWithoutNotify(port);
-        });
-    }
-
-    private void OnStartClientButtonActivated(ClickEvent e)
-    {
-        log.Debug("Start Client button activated");
-    }
-
-    private void OnStartServerButtonActivated(ClickEvent e)
-    {
-        log.Debug("Start Server button activated");
-    }
-
-    private void OnStartClientServerButtonActivated(ClickEvent e)
-    {
-        log.Debug("Start Client and Server button activated");
-
-        sceneLoader.LoadAdditiveSceneAsync("Server", gameObject.scene);
-
-        for (int i = 0; i < 10; i++)
-        {
-            sceneLoader.LoadAdditiveSceneAsync("Client", gameObject.scene);
+            this.log = log;
+            this.gameVersion = gameVersion;
+            this.sceneLoader = sceneLoader;
         }
 
-        document.enabled = false;
-    }
+        private void Start()
+        {
+            document = GetComponent<UIDocument>();
+            root = document.rootVisualElement;
+
+            DisplayVersion();
+
+            RegisterTextFields();
+            RegisterButtons();
+        }
+
+        private void DisplayVersion()
+        {
+            root.Query<Label>("version").ForEach((label) =>
+            {
+                label.text = $"{gameVersion.Company}.{gameVersion.Product} {gameVersion.Version}";
+            });
+        }
+
+        private void RegisterTextFields()
+        {
+            root.Query<TextField>().ForEach((field) =>
+            {
+                switch (field.name)
+                {
+                    case "username-field":
+                        field.value = username;
+                        field.RegisterValueChangedCallback(OnUsernameFieldChanged); break;
+
+                    case "address-field":
+                        field.value = address;
+                        field.RegisterValueChangedCallback(OnAddressFieldChanged); break;
+
+                    case "port-field":
+                        field.value = port;
+                        field.RegisterValueChangedCallback(OnPortFieldChanged); break;
+
+                    default: break;
+                }
+            });
+        }
+
+        private void RegisterButtons()
+        {
+            root.Query<Button>().ForEach((button) =>
+            {
+                switch (button.name)
+                {
+                    case "start-client-button":
+                        button.RegisterCallback<ClickEvent>(OnStartClientButtonActivated); break;
+
+                    case "start-server-button":
+                        button.RegisterCallback<ClickEvent>(OnStartServerButtonActivated); break;
+
+                    case "start-client-server-button":
+                        button.RegisterCallback<ClickEvent>(OnStartClientServerButtonActivated); break;
+
+                    default: break;
+                }
+            });
+        }
+
+        private void OnUsernameFieldChanged(ChangeEvent<string> e)
+        {
+            username = e.newValue;
+
+            root.Query<TextField>("username-field").ForEach((field) =>
+            {
+                field.SetValueWithoutNotify(username);
+            });
+        }
+
+        private void OnAddressFieldChanged(ChangeEvent<string> e)
+        {
+            address = e.newValue;
+
+            root.Query<TextField>("address-field").ForEach((field) =>
+            {
+                field.SetValueWithoutNotify(address);
+            });
+        }
+
+        private void OnPortFieldChanged(ChangeEvent<string> e)
+        {
+            port = e.newValue;
+
+            root.Query<TextField>("port-field").ForEach((field) =>
+            {
+                field.SetValueWithoutNotify(port);
+            });
+        }
+
+        private void OnStartClientButtonActivated(ClickEvent e)
+        {
+            log.Debug("Start Client button activated");
+        }
+
+        private void OnStartServerButtonActivated(ClickEvent e)
+        {
+            log.Debug("Start Server button activated");
+        }
+
+        private void OnStartClientServerButtonActivated(ClickEvent e)
+        {
+            log.Debug("Start Client and Server button activated");
+
+            sceneLoader.LoadAdditiveSceneAsync("Server", gameObject.scene);
+
+            for (int i = 0; i < 10; i++)
+            {
+                sceneLoader.LoadAdditiveSceneAsync("Client", gameObject.scene);
+            }
+
+            document.enabled = false;
+        }
+    } 
 }
