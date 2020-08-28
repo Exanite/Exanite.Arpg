875a72e5fd3f05ecde9c917cf670aef2a068ae3a
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-27 17:30:40 -0700

    Add GameStartSettings

---
+++ Assets/Prototype/Networking/Startup/GameStartSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Prototype.Networking
+{
+    public class GameStartSettings // likely will split into Server and Client implementations later on
+    {
+        public GameType gameType;
+
+        public ushort port;
+        public string username;
+        public IPAddress address;
+
+        public GameStartSettings(GameType gameType, ushort port, string username, IPAddress address)
+        {
+            this.gameType = gameType;
+            this.port = port;
+            this.username = username;
+            this.address = address;
+        }
+
+        public enum GameType
+        {
+            Server,
+            Client,
+        }
+    } 
+}
