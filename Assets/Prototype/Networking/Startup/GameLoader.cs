2ddcf77a73d4cde21df522655b07a0531894c46a
Exanite <42710136+Exanite@users.noreply.github.com>
2020-08-27 17:23:44 -0700

    Move GameLoader to Prototype.Networking.Startup

---
+++ Assets/Prototype/Networking/Startup/GameLoader.cs
@@ -0,0 +1,55 @@
+﻿using Exanite.Arpg.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Prototype.Networking
+{
+    public class GameLoader : MonoBehaviour
+    {
+        public string serverSceneName = "Server";
+        public string clientSceneName = "Client";
+
+        public int numberOfClients = 1;
+
+        private ILog log;
+
+        [Inject]
+        public void Inject(ILog log)
+        {
+            this.log = log;
+        }
+
+        private void Awake()
+        {
+            log.Information("Disabling physics auto-simulation");
+
+            Physics.autoSimulation = false;
+        }
+
+        private void Start()
+        {
+            CreateServer();
+
+            for (int i = 0; i < numberOfClients; i++)
+            {
+                CreateClient();
+            }
+        }
+
+        public void CreateServer()
+        {
+            var loadSceneParameters = new LoadSceneParameters(LoadSceneMode.Additive, LocalPhysicsMode.Physics3D);
+
+            SceneManager.LoadScene(serverSceneName, loadSceneParameters);
+        }
+
+        [ContextMenu("Create new Client")]
+        public void CreateClient()
+        {
+            var loadSceneParameters = new LoadSceneParameters(LoadSceneMode.Additive, LocalPhysicsMode.Physics3D);
+
+            SceneManager.LoadScene(clientSceneName, loadSceneParameters);
+        }
+    }
+}
