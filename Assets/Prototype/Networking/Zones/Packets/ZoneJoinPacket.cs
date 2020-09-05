bddf712a5845bccf08347a9ddd9891fcaff6d80f
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-04 22:10:36 -0700

    Add ZoneJoin packet

---
+++ Assets/Prototype/Networking/Zones/Packets/ZoneJoinPacket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Exanite.Arpg.Networking;
+using LiteNetLib.Utils;
+using Prototype.Networking.Players;
+
+namespace Prototype.Networking.Zones.Packets
+{
+    public class ZoneJoinPacket : IPacket
+    {
+        public Guid guid;
+
+        public PlayerCreateData localPlayer = new PlayerCreateData();
+        public List<PlayerCreateData> zonePlayers = new List<PlayerCreateData>();
+
+        public void Deserialize(NetDataReader reader)
+        {
+            guid = reader.GetGuid();
+            localPlayer.Deserialize(reader);
+
+            zonePlayers.Clear();
+            int count = reader.GetInt();
+            for (int i = 0; i < count; i++)
+            {
+                zonePlayers.Add(reader.Get<PlayerCreateData>());
+            }
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(guid);
+            localPlayer.Serialize(writer);
+
+            writer.Put(zonePlayers.Count);
+            for (int i = 0; i < zonePlayers.Count; i++)
+            {
+                writer.Put(zonePlayers[i]);
+            }
+        }
+    }
+}
