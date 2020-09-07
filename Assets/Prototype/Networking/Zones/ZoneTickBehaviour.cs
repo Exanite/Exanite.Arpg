9bf7384a4d8f5547115f17b4345a46eac83163b0
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-06 19:10:15 -0700

    Add ZoneTickBehaviour

---
+++ Assets/Prototype/Networking/Zones/ZoneTickBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Prototype.Networking.Zones
+{
+    [DefaultExecutionOrder(-50)]
+    public class ZoneTickBehaviour : MonoBehaviour
+    {
+        private Zone zone;
+
+        [Inject]
+        public void Inject(Zone zone)
+        {
+            this.zone = zone;
+        }
+
+        private void FixedUpdate()
+        {
+            zone.tick++;
+        }
+    }
+}
