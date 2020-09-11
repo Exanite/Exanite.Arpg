18e30fbc4aa3d1b1547e93491ca3389667e6aa83
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-10 21:02:48 -0700

    Add ReconciliationData

---
+++ Assets/Prototype/Networking/Players/ReconciliationData.cs
@@ -0,0 +1,12 @@
+﻿using Prototype.Networking.Players.Data;
+
+namespace Prototype.Networking.Players
+{
+    public struct ReconciliationData
+    {
+        public uint tick;
+
+        public PlayerInputData inputData;
+        public PlayerUpdateData updateData;
+    }
+}
