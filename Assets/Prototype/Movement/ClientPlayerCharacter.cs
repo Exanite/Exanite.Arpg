0b9a9966c3e5787f1fb7fc5598decf11f4bc13fa
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-14 01:14:20 -0700

    Implement ClientPlayerCharacter

--- Assets/Prototype/Movement/ClientPlayerCharacter.cs
+++ Assets/Prototype/Movement/ClientPlayerCharacter.cs
@@ -7,25 +7,45 @@ namespace Prototype.Movement
     {
         public ServerPlayerCharacter server;
         public uint tick;
+        public float mapSize = 10;
+
+        private PlayerUpdateData currentUpdateData;
 
         private RingBuffer<PlayerUpdateData> updateBuffer;
+
+        private PlayerInput input;
+        private PlayerLogic logic;
+        private PlayerInterpolation interpolation;
 
         private void Start()
         {
             updateBuffer = new RingBuffer<PlayerUpdateData>(64);
+
+            input = new PlayerInput();
+            logic = new PlayerLogic(mapSize);
+            interpolation = new PlayerInterpolation(transform);
+        }
+
+        private void Update()
+        {
+            interpolation.Update(tick);
         }
 
         private void FixedUpdate()
         {
             // input
+            var inputData = input.Get();
 
             // state
 
             // simulation
+            currentUpdateData = logic.Simulate(currentUpdateData, inputData);
 
             // view
+            interpolation.UpdateData(currentUpdateData, tick);
 
             // messaging
+            server.ReceivePlayerInput(inputData);
 
             tick++;
         }
