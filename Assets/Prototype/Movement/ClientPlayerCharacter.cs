9bd83555c34b880772f15ef4de2e43e152cc626a
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-14 00:49:03 -0700

    Add PlayerCharacter classes

---
+++ Assets/Prototype/Movement/ClientPlayerCharacter.cs
@@ -0,0 +1,38 @@
+﻿using Prototype.Networking.Players.Data;
+using UnityEngine;
+
+namespace Prototype.Movement
+{
+    public class ClientPlayerCharacter : MonoBehaviour
+    {
+        public ServerPlayerCharacter server;
+        public uint tick;
+
+        private RingBuffer<PlayerUpdateData> updateBuffer;
+
+        private void Start()
+        {
+            updateBuffer = new RingBuffer<PlayerUpdateData>(64);
+        }
+
+        private void FixedUpdate()
+        {
+            // input
+
+            // state
+
+            // simulation
+
+            // view
+
+            // messaging
+
+            tick++;
+        }
+
+        public void ReceivePlayerUpdate(PlayerUpdateData data)
+        {
+            updateBuffer.Enqueue(data);
+        }
+    }
+}
---
+++ Assets/Prototype/Movement/ServerPlayerCharacter.cs
@@ -0,0 +1,36 @@
+﻿using Prototype.Networking.Players.Data;
+using UnityEngine;
+
+namespace Prototype.Movement
+{
+    public class ServerPlayerCharacter : MonoBehaviour
+    {
+        public ClientPlayerCharacter client;
+        public uint tick;
+
+        private RingBuffer<PlayerInputData> inputBuffer;
+
+        private void Start()
+        {
+            inputBuffer = new RingBuffer<PlayerInputData>(64);
+        }
+
+        private void FixedUpdate()
+        {
+            // input
+
+            // state
+
+            // simulation
+
+            // messaging
+
+            tick++;
+        }
+
+        public void ReceivePlayerInput(PlayerInputData data)
+        {
+            inputBuffer.Enqueue(data);
+        }
+    }
+}
