a5da414fe6aba9644d9a7c099732013f1c7bcf77
Exanite <42710136+Exanite@users.noreply.github.com>
2020-09-14 00:49:39 -0700

    Add PlayerInterpolation

---
+++ Assets/Prototype/Movement/PlayerInterpolation.cs
@@ -0,0 +1,39 @@
+﻿using Prototype.Networking.Players.Data;
+using UnityEngine;
+
+namespace Prototype.Movement
+{
+    public class PlayerInterpolation
+    {
+        public readonly Transform player;
+
+        public PlayerUpdateData current;
+        public PlayerUpdateData previous;
+        public uint lastUpdateTick;
+
+        public PlayerInterpolation(Transform player)
+        {
+            this.player = player;
+        }
+
+        public void Update(uint currentTick)
+        {
+            uint ticksSinceLastUpdate = currentTick - lastUpdateTick;
+            float timePerTick = Time.fixedDeltaTime;
+            float timeSinceLastTick = Time.time - Time.fixedTime;
+
+            float timeSinceLastUpdate = ticksSinceLastUpdate * timePerTick + timeSinceLastTick;
+            float t = timeSinceLastUpdate / timePerTick;
+
+            player.position = Vector3.LerpUnclamped(previous.playerPosition, previous.playerPosition, t);
+        }
+
+        public void UpdateData(PlayerUpdateData newData, uint tick)
+        {
+            previous = current;
+            current = newData;
+
+            lastUpdateTick = tick;
+        }
+    }
+}
